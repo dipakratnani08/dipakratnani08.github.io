07ba2125cc62da31c81a8cc2c7861a90acf86e31
Dipak Ratnani <dipakratnani08@gmail.com>
2026-05-06 18:52:51 +0530

    save changes

---
+++ vite.config.github.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  base: "/",
+  plugins: [],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "src"),
+    },
+  },
+  root: path.resolve(import.meta.dirname),
+  build: {
+    outDir: path.resolve(import.meta.dirname, "dist"),
+    emptyOutDir: true,
+  },
+});
